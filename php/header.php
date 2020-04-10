11336f5fabe993d9819eed234a6cbe3843d05484
Lucasca95 <lucasca95@yahoo.com.ar>
2020-04-10 20:54:33 -0300

    comun.css name changed

--- php/header.php
+++ php/header.php
@@ -1,7 +1,3 @@
-
-
-
-
 <nav class="navbar navbar-expand-lg navbar-dark navbar-custom">
     <span class="navbar-brand">&nbsp PHP-MySQL-Docker&nbsp</span>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
